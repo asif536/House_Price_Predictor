d33d3a7dd50cb17df46f7578ad426c795d2f14ce
Mohd Asif <asif536shah@gmail.com>
2017-10-20 13:50:58 +0530

    Adding Regularization

--- src/function/cost.py
+++ src/function/cost.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 # Compute the Cost function
-def CostFuction(X,Y,m,theta):
+def CostFuction(X,Y,m,theta,lamda):
 	j=0.0
 	hypothysis		= 	X.dot(theta)
 	sqr_diff		=	np.power((hypothysis-Y),2)
-	j				=	(1.0/(2*m))*np.sum(sqr_diff)  
+	j			=	(1.0/(2*m))*np.sum(sqr_diff)+(1.0/(2*m))*lamda*np.sum(theta**2)
 	return j
