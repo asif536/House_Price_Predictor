1a789cbb3d824589ad7231ad99d5656c1ab2ce86
Mohd Asif <asif536shah@gmail.com>
2017-10-20 13:51:57 +0530

    Adding regularization

--- src/function/gradiant_desent.py
+++ src/function/gradiant_desent.py
@@ -3,15 +3,15 @@ from cost import CostFuction
 
 
 # Calculating gradiant descent and minimize the CostFuction
-def Gradiant_descent(x,y,theta,m,learning_rate,num_iters):
+def Gradiant_descent(x,y,theta,m,learning_rate,num_iters,lamda):
 	j_history = np.zeros((num_iters,1))
 	j_history = j_history.reshape(num_iters,1)
 	for i in range(num_iters):
 		hypothysis		=	x.dot(theta)
 		diff			=	(hypothysis-y)
 		gradiant 		=	(x.T).dot(diff)/m
-		theta			=	theta-(learning_rate*gradiant)
-		j_history[i]	=	CostFuction(x,y,m,theta)
+		theta			=	theta*(1-((learning_rate*lamda)/m))-(learning_rate*gradiant)
+		j_history[i]		=	CostFuction(x,y,m,theta,lamda)
 		print j_history[i]
 	return [theta,j_history]
 
