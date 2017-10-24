8e40635f821587bff3adfceee9ad0f41c0dd3193
Mohd Asif <asif536shah@gmail.com>
2017-10-24 22:21:00 +0530

    Final Commit

--- src/function/feature_scaling.py
+++ src/function/feature_scaling.py
@@ -4,7 +4,12 @@ import numpy as np
 def FeatureScaling(x):
 	for i in range(1,7):
 		mean 	= np.mean(x[:,i])
-		std		= np.std(x[:,i])
+		std	= np.std(x[:,i])
 		x[:,i]	= (x[:,i]-mean)/std
 
 	return x
+
+def FeatureScalingInput(input_x,X):
+	for i in range(1,7):
+		input_x[i] = float((input_x[i]-np.mean(X[:,i]))/np.std(X[:,i]))
+	return input_x
