add35591652f3ded8dec80be4cf497ee5ee17278
Mohd Asif <asif536shah@gmail.com>
2017-10-24 22:28:03 +0530

    Final Commit

--- src/function/input_data.py
+++ src/function/input_data.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-def InputData(theta):
+def InputData():
 	li=[1]
-	print "Enter the No of Bedrooms,No of Bathrooms,Sqft_living,Sqft_lot,No of floors,zipcode(eg 98010)\n"
+	print"Enter the No of Bedrooms,No of Bathrooms,Sqft_living,Sqft_lot,No of floors,zipcode(eg 98010)\n"
 	for i in range(1,7):
-		accept=input()
+		accept=float(input())
 		li.append(accept)
 		x=np.asarray(li)
-	predict=np.dot(x,theta)
-	return predict
+	return x
 	
 	
 	
